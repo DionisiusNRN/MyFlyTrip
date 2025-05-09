78179e010e649ff139aa780feccccbbe3354e9c7
dionisiusnusaca <dnur3no@gmail.com>
2025-05-09 12:00:19 +0700

    mengubah source asset menjadi lokal

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 <body class="flex justify-center items-center min-h-screen bg-gray-100">
     <div class="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
