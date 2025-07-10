f263799b565d80e2f23c545927cdf553276a0871
dionisiusnusaca <dnur3no@gmail.com>
2025-07-10 21:46:13 +0700

    add documentation

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,16 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 class Authenticate
 {
     /**
-     * Handle an incoming request.
+     * Middleware ini memeriksa apakah user sudah login atau belum.
+     * Guard default yang digunakan adalah 'user_customer', sesuai sistem auth custom.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $guard - (optional) nama guard yang digunakan. Default: user_customer
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next, string $guard = 'user_customer'): Response
     {
+        // Kalau user belum login, arahkan ke halaman login
         if (!Auth::guard($guard)->check()) {
             return redirect()->route('login.form');
         }
 
+        // Kalau user sudah login, lanjutkan ke proses berikutnya (next middleware / controller)
         return $next($request);
     }
 }
